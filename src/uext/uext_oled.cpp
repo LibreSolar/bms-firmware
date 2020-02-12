345b8550aebef3cb442ec1d85838c769c136ab85
Martin Jaeger <17674105+martinjaeger@users.noreply.github.com>
2020-02-12 18:03:43 +0100

    Replace OLED lib with submodule

--- src/uext/uext_oled.cpp
+++ src/uext/uext_oled.cpp
@@ -20,14 +20,13 @@
 
 #ifdef OLED_ENABLED     // otherwise don't compile code to reduce firmware size
 
+#include "oled_ssd1306.h"
+
 #include "pcb.h"
-
-#include "Adafruit_SSD1306.h" // Library for SSD1306 OLED-Display, 128x64
-
 #include "bms.h"
 
 I2C i2c(PIN_UEXT_SDA, PIN_UEXT_SCL);
-Adafruit_SSD1306_I2c oled(i2c, PB_2, 0x78, 64, 128);
+OledSSD1306 oled(i2c);
 
 extern bool blinkOn;
 
@@ -35,30 +34,48 @@ extern BmsStatus bms_status;
 
 extern float load_voltage;
 
-void uext_init() {;}    // not needed
+void uext_init()
+{
+#ifdef OLED_BRIGHTNESS
+    oled.init(OLED_BRIGHTNESS);
+#else
+    oled.init();        // use default (lowest brightness)
+#endif
+}
 
 void uext_process_1s()    // OLED SSD1306
 {
+    char buf[30];
+    unsigned int len;
+
     i2c.frequency(400000);
-    oled.clearDisplay();
+    oled.clear();
 
     oled.setTextCursor(0, 0);
-    oled.printf("%.2f V", bms_status.pack_voltage);
+    len = snprintf(buf, sizeof(buf), "%.2f V", bms_status.pack_voltage);
+    oled.writeString(buf, len);
+
     oled.setTextCursor(64, 0);
-    oled.printf("%.2f A", bms_status.pack_current);
+    len = snprintf(buf, sizeof(buf), "%.2f A", bms_status.pack_current);
+    oled.writeString(buf, len);
 
     oled.setTextCursor(0, 8);
-    oled.printf("T:%.1f C", bms_status.bat_temp_avg);
+    len = snprintf(buf, sizeof(buf), "T:%.1f C", bms_status.bat_temp_avg);
+    oled.writeString(buf, len);
+
     oled.setTextCursor(64, 8);
-    oled.printf("SOC:%d", bms_status.soc);
+    len = snprintf(buf, sizeof(buf), "SOC:%d", bms_status.soc);
+    oled.writeString(buf, len);
 
     oled.setTextCursor(0, 16);
-    oled.printf("Load: %.2fV", load_voltage);
+    len = snprintf(buf, sizeof(buf), "Load: %.2fV", load_voltage);
+    oled.writeString(buf, len);
 
     for (int i = 0; i < NUM_CELLS_MAX; i++) {
         if (blinkOn || !(bms_status.balancing_status & (1 << i))) {
             oled.setTextCursor((i % 2 == 0) ? 0 : 64, 24 + (i / 2) * 8);
-            oled.printf("%d:%.3f V", i+1, bms_status.cell_voltages[i]);
+            len = snprintf(buf, sizeof(buf), "%d:%.3f V", i+1, bms_status.cell_voltages[i]);
+            oled.writeString(buf, len);
         }
     }
 
