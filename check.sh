ca7fa6470577f4bf25e246fc42b0664d65853ed2
Martin Jäger <martin@libre.solar>
2020-04-29 18:00:24 +0200

    Add check.sh and include bq769x0_zephyr in Travis

---
+++ check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# This file performs the same checks as TravisCI and should be run after each commit and before
+# pushing the commits
+#
+
+echo "---------- Trailing whitespace check -------------"
+git diff --check `git rev-list HEAD | tail -n 1`..$TRAVIS_BRANCH
+if [ $? != 0 ]; then
+        exit 1;
+fi
+
+echo "---------- Running compile check -------------"
+platformio run -e bms_5s50_sc -e bms_15s80_sc_bq76930_zephyr -e bms_8s50_ic_l452
+if [ $? != 0 ]; then
+        exit 1;
+fi
+
+echo "---------- Running unit-tests -------------"
+platformio test -e unit_test_isl94202 -e unit_test_bq769x0
+if [ $? != 0 ]; then
+        exit 1;
+fi
+
+echo "---------- Running static code checks -------------"
+platformio check -e bms_5s50_sc -e bms_8s50_ic_l452
+if [ $? != 0 ]; then
+        exit 1;
+fi
+
+echo "---------- All tests passed -------------"
