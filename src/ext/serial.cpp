dbf7adf1e6734d6ecd31b7fa31f7e9afa357b4fa
Martin Jäger <martin@libre.solar>
2021-10-05 12:09:15 +0200

    Serial: Use semaphores instead of frequent polling
    This allows the serial thread to sleep and avoids polling for incoming
    commands every 10 ms as it was done before.
    
    Two semaphores are used, one to lock the receive buffer and the other
    one to signal a received command.
    
    Also improving some variable and function naming.
    
    Commit based on charge controller repo.

--- src/ext/serial.cpp
+++ src/ext/serial.cpp
@@ -27,55 +27,54 @@
 
 const struct device *uart_dev = device_get_binding(UART_DEVICE_NAME);
 
-static char buf_resp[CONFIG_THINGSET_SERIAL_TX_BUF_SIZE];
-static char buf_req[CONFIG_THINGSET_SERIAL_RX_BUF_SIZE];
+static char tx_buf[CONFIG_THINGSET_SERIAL_TX_BUF_SIZE];
+static char rx_buf[CONFIG_THINGSET_SERIAL_RX_BUF_SIZE];
 
-static volatile size_t req_pos = 0;
-static volatile bool command_flag = false;
+static volatile size_t rx_buf_pos = 0;
+
+static struct k_sem command_flag;       // used as an event to signal a received command
+static struct k_sem rx_buf_mutex;       // binary semaphore used as mutex in ISR context
 
 extern ThingSet ts;
 
 const char serial_subset_path[] = "serial";
 static ThingSetDataObject *serial_subset;
 
-void process_1s()
+void serial_pub_msg()
 {
     if (pub_serial_enable) {
-        int len = ts.txt_statement(buf_resp, sizeof(buf_resp), serial_subset);
+        int len = ts.txt_statement(tx_buf, sizeof(tx_buf), serial_subset);
         for (int i = 0; i < len; i++) {
-            uart_poll_out(uart_dev, buf_resp[i]);
+            uart_poll_out(uart_dev, tx_buf[i]);
         }
         uart_poll_out(uart_dev, '\n');
     }
 }
 
-void process_asap()
+void serial_process_command()
 {
-    if (command_flag) {
-        // commands must have 2 or more characters
-        if (req_pos > 1) {
-            printf("Received Request (%d bytes): %s\n", strlen(buf_req), buf_req);
+    // commands must have 2 or more characters
+    if (rx_buf_pos > 1) {
+        printf("Received Request (%d bytes): %s\n", strlen(rx_buf), rx_buf);
 
-            int len = ts.process((uint8_t *)buf_req, strlen(buf_req),
-                (uint8_t *)buf_resp, sizeof(buf_resp));
+        int len = ts.process((uint8_t *)rx_buf, strlen(rx_buf),
+            (uint8_t *)tx_buf, sizeof(tx_buf));
 
-            for (int i = 0; i < len; i++) {
-                uart_poll_out(uart_dev, buf_resp[i]);
-            }
-            uart_poll_out(uart_dev, '\n');
+        for (int i = 0; i < len; i++) {
+            uart_poll_out(uart_dev, tx_buf[i]);
         }
-
-        // start listening for new commands
-        command_flag = false;
-        req_pos = 0;
+        uart_poll_out(uart_dev, '\n');
     }
+
+    // release buffer and start waiting for new commands
+    rx_buf_pos = 0;
+    k_sem_give(&rx_buf_mutex);
 }
 
-/**
- * Read characters from stream until line end \n detected, signal command available then
- * and wait for processing
+/*
+ * Read characters from stream until line end \n is detected, afterwards signal available command.
  */
-void process_input(const struct device *dev, void* user_data)
+void serial_cb(const struct device *dev, void* user_data)
 {
     uint8_t c = 0;
 
@@ -83,55 +82,64 @@ void process_input(const struct device *dev, void* user_data)
         return;
     }
 
-    while (uart_irq_rx_ready(uart_dev) && command_flag == false) {
+    while (uart_irq_rx_ready(uart_dev) && k_sem_take(&rx_buf_mutex, K_NO_WAIT) == 0) {
+
         uart_fifo_read(uart_dev, &c, 1);
 
         // \r\n and \n are markers for line end, i.e. command end
         // we accept this at any time, even if the buffer is 'full', since
         // there is always one last character left for the \0
         if (c == '\n') {
-            if (req_pos > 0 && buf_req[req_pos-1] == '\r') {
-                buf_req[req_pos-1] = '\0';
+            if (rx_buf_pos > 0 && rx_buf[rx_buf_pos-1] == '\r') {
+                rx_buf[rx_buf_pos-1] = '\0';
             }
             else {
-                buf_req[req_pos] = '\0';
+                rx_buf[rx_buf_pos] = '\0';
             }
-            // start processing
-            command_flag = true;
+            // start processing command and keep the rx_buf_mutex locked
+            k_sem_give(&command_flag);
+            return;
         }
         // backspace allowed if there is something in the buffer already
-        else if (req_pos > 0 && c == '\b') {
-            req_pos--;
+        else if (rx_buf_pos > 0 && c == '\b') {
+            rx_buf_pos--;
         }
         // Fill the buffer up to all but 1 character (the last character is reserved for '\0')
         // Characters beyond the size of the buffer are dropped.
-        else if (req_pos < (sizeof(buf_req)-1)) {
-            buf_req[req_pos++] = c;
+        else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
+            rx_buf[rx_buf_pos++] = c;
         }
+
+        k_sem_give(&rx_buf_mutex);
     }
 }
 
 void serial_thread()
 {
-    uint32_t last_call = 0;
+    k_sem_init(&command_flag, 0, 1);
+    k_sem_init(&rx_buf_mutex, 1, 1);
 
-    uart_irq_callback_user_data_set(uart_dev, process_input, NULL);
+    __ASSERT_NO_MSG(uart_dev != NULL);
+
+    uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
     uart_irq_rx_enable(uart_dev);
 
     serial_subset = ts.get_endpoint(serial_subset_path, strlen(serial_subset_path));
 
-    while (true) {
-        uint32_t now = k_uptime_get() / 1000;
+    int64_t t_start = k_uptime_get();
 
-        process_asap();     // approx. every millisecond
-        if (now >= last_call + 1) {
-            last_call = now;
-            process_1s();
+    while (true) {
+        if (k_sem_take(&command_flag, K_TIMEOUT_ABS_MS(t_start)) == 0) {
+            serial_process_command();
         }
-        k_sleep(K_MSEC(10));
+        else {
+            // semaphore timed out (should happen exactly every 1 second)
+            t_start += 1000;
+            serial_pub_msg();
+        }
     }
 }
 
-K_THREAD_DEFINE(serial, 1280, serial_thread, NULL, NULL, NULL, 6, 0, 1000);
+K_THREAD_DEFINE(serial_thread_id, 1280, serial_thread, NULL, NULL, NULL, 6, 0, 1000);
 
 #endif /* CONFIG_THINGSET_SERIAL */
