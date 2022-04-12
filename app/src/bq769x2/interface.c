ff4652a767e642ff69179714a72c84e5e67e49a0
Martin Jäger <martin@libre.solar>
2022-04-12 17:52:51 +0200

    bq769x2: improve error handling in I2C interface

--- app/src/bq769x2/interface.c
+++ app/src/bq769x2/interface.c
@@ -69,22 +69,27 @@ int bq769x2_direct_read_u2(const uint8_t reg_addr, uint16_t *value)
 {
     uint8_t buf[2];
 
-    int ret = bq769x2_read_bytes(reg_addr, buf, 2);
+    int err = bq769x2_read_bytes(reg_addr, buf, 2);
+    if (err) {
+        LOG_ERR("direct_read_u2 failed");
+    }
+    else {
+        *value = buf[0] | buf[1] << 8;      // little-endian byte order
+    }
 
-    *value = buf[0] | buf[1] << 8;      // little-endian byte order
-
-    return ret;
+    return err;
 }
 
 int bq769x2_direct_read_i2(const uint8_t reg_addr, int16_t *value)
 {
     uint16_t u16;
 
-    int ret = bq769x2_direct_read_u2(reg_addr, &u16);
+    int err = bq769x2_direct_read_u2(reg_addr, &u16);
+    if (!err) {
+        *value = *(int16_t *)&u16;
+    }
 
-    *value = *(int16_t *)&u16;
-
-    return ret;
+    return err;
 }
 
 static int bq769x2_subcmd_read(const uint16_t subcmd, uint32_t *value, const size_t num_bytes)
@@ -94,24 +99,33 @@ static int bq769x2_subcmd_read(const uint16_t subcmd, uint32_t *value, const siz
 
     // write the subcommand we want to read data from
     uint8_t buf_subcmd[2] = { subcmd & 0x00FF, subcmd >> 8 };
-    bq769x2_write_bytes(BQ769X2_CMD_SUBCMD_LOWER, buf_subcmd, 2);
+    err = bq769x2_write_bytes(BQ769X2_CMD_SUBCMD_LOWER, buf_subcmd, 2);
+    if (err) {
+        goto err;
+    }
 
     k_usleep(500);  // most subcommands need approx 500 us to complete
 
     // wait until data is ready
     int num_tries = 0;
     do {
-        bq769x2_read_bytes(BQ769X2_CMD_SUBCMD_LOWER, buf_data, 2);
-        num_tries++;
-        if (num_tries > 10) {
+        err = bq769x2_read_bytes(BQ769X2_CMD_SUBCMD_LOWER, buf_data, 2);
+        if (err) {
+            goto err;
+        }
+        else if (num_tries > 10) {
             LOG_ERR("Reached maximum number of tries to read subcommand");
             return -EIO;
         }
+        num_tries++;
     } while (buf_subcmd[0] != buf_data[0] || buf_subcmd[1] != buf_data[1]);
 
     // read data length
     uint8_t data_length;
     err = bq769x2_read_bytes(BQ769X2_SUBCMD_DATA_LENGTH, &data_length, 1);
+    if (err) {
+        goto err;
+    }
     data_length = data_length - 4;  // substract subcmd + checksum + length bytes
     if (err || data_length > 0x20 || num_bytes > 4) {
         LOG_ERR("Subcmd data length 0x%X or num_bytes invalid", data_length);
@@ -121,7 +135,10 @@ static int bq769x2_subcmd_read(const uint16_t subcmd, uint32_t *value, const siz
     // read all available data (needed for checksum calculation, may be more than num_bytes)
     *value = 0;
     uint8_t checksum = buf_subcmd[0] + buf_subcmd[1];
-    bq769x2_read_bytes(BQ769X2_SUBCMD_DATA_START, buf_data, data_length);
+    err = bq769x2_read_bytes(BQ769X2_SUBCMD_DATA_START, buf_data, data_length);
+    if (err) {
+        goto err;
+    }
     for (int i = 0; i < data_length; i++) {
         if (i < num_bytes) {
             *value += buf_data[i] << (i * 8);
@@ -131,18 +148,26 @@ static int bq769x2_subcmd_read(const uint16_t subcmd, uint32_t *value, const siz
     checksum = ~checksum;
 
     // validate data with checksum
-    bq769x2_read_bytes(BQ769X2_SUBCMD_DATA_CHECKSUM, buf_data, 1);
-    if (buf_data[0] != checksum) {
+    err = bq769x2_read_bytes(BQ769X2_SUBCMD_DATA_CHECKSUM, buf_data, 1);
+    if (err) {
+        goto err;
+    }
+    else if (buf_data[0] != checksum) {
         LOG_ERR("Subcmd checksum incorrect: calculated 0x%X, read 0x%X", checksum, buf_data[0]);
         return -EIO;
     }
 
     return 0;
+
+err:
+    LOG_ERR("I2C error for subcmd_read: %d", err);
+    return err;
 }
 
 static int bq769x2_subcmd_write(const uint16_t subcmd, const uint32_t value, const size_t num_bytes)
 {
     uint8_t buf_data[4];
+    int err;
 
     if (num_bytes > 4) {
         LOG_ERR("Subcmd num_bytes 0x%X invalid", num_bytes);
@@ -151,7 +176,10 @@ static int bq769x2_subcmd_write(const uint16_t subcmd, const uint32_t value, con
 
     // write the subcommand we want to write data to
     uint8_t buf_subcmd[2] = { subcmd & 0x00FF, subcmd >> 8 };
-    bq769x2_write_bytes(BQ769X2_CMD_SUBCMD_LOWER, buf_subcmd, 2);
+    err = bq769x2_write_bytes(BQ769X2_CMD_SUBCMD_LOWER, buf_subcmd, 2);
+    if (err) {
+        goto err;
+    }
 
     // write actual data and calculate checksum
     uint8_t checksum = buf_subcmd[0] + buf_subcmd[1];
@@ -160,63 +188,95 @@ static int bq769x2_subcmd_write(const uint16_t subcmd, const uint32_t value, con
         checksum += buf_data[i];
     }
     checksum = ~checksum;
-    bq769x2_write_bytes(BQ769X2_SUBCMD_DATA_START, buf_data, num_bytes);
+    err = bq769x2_write_bytes(BQ769X2_SUBCMD_DATA_START, buf_data, num_bytes);
+    if (err) {
+        goto err;
+    }
 
     // write checksum and data length as one word
     buf_data[0] = checksum;
     buf_data[1] = num_bytes + 4;
-    bq769x2_write_bytes(BQ769X2_SUBCMD_DATA_CHECKSUM, buf_data, 2);
+    err = bq769x2_write_bytes(BQ769X2_SUBCMD_DATA_CHECKSUM, buf_data, 2);
+    if (err) {
+        goto err;
+    }
 
     return 0;
+
+err:
+    LOG_ERR("I2C error for subcmd_read: %d", err);
+    return err;
 }
 
 int bq769x2_subcmd_read_u1(const uint16_t subcmd, uint8_t *value)
 {
     uint32_t u32;
-    int ret = bq769x2_subcmd_read(subcmd, &u32, 1);
-    *value = (uint8_t)u32;
-    return ret;
+    int err = bq769x2_subcmd_read(subcmd, &u32, 1);
+    if (!err) {
+        *value = (uint8_t)u32;
+    }
+    return err;
 }
 
 int bq769x2_subcmd_read_u2(const uint16_t subcmd, uint16_t *value)
 {
     uint32_t u32;
-    int ret = bq769x2_subcmd_read(subcmd, &u32, 2);
-    *value = (uint16_t)u32;
-    return ret;
+    int err = bq769x2_subcmd_read(subcmd, &u32, 2);
+    if (!err) {
+        *value = (uint16_t)u32;
+    }
+    return err;
 }
 
 int bq769x2_subcmd_read_u4(const uint16_t subcmd, uint32_t *value)
 {
-    return bq769x2_subcmd_read(subcmd, value, 4);
+    uint32_t u32;
+    int err = bq769x2_subcmd_read(subcmd, &u32, 4);
+    if (!err) {
+        *value = u32;
+    }
+    return err;
 }
 
 int bq769x2_subcmd_read_i1(const uint16_t subcmd, int8_t *value)
 {
     uint32_t u32;
-    int ret = bq769x2_subcmd_read(subcmd, &u32, 1);
-    *value = *(int8_t *)&u32;
-    return ret;
+    int err = bq769x2_subcmd_read(subcmd, &u32, 1);
+    if (!err) {
+        *value = *(int8_t *)&u32;
+    }
+    return err;
 }
 
 int bq769x2_subcmd_read_i2(const uint16_t subcmd, int16_t *value)
 {
     uint32_t u32;
-    uint16_t u16;
-    int ret = bq769x2_subcmd_read(subcmd, &u32, 2);
-    u16 = (uint16_t)u32;    // doing narrowing conversion first avoids compiler warnings
-    *value = *(int16_t *)&u16;
-    return ret;
+    int err = bq769x2_subcmd_read(subcmd, &u32, 2);
+    if (!err) {
+        uint16_t u16 = (uint16_t)u32; // doing narrowing conversion first avoids compiler warnings
+        *value = *(int16_t *)&u16;
+    }
+    return err;
 }
 
 int bq769x2_subcmd_read_i4(const uint16_t subcmd, int32_t *value)
 {
-    return bq769x2_subcmd_read(subcmd, (uint32_t *)value, 4);
+    int32_t i32;
+    int err = bq769x2_subcmd_read(subcmd, (uint32_t *)&i32, 4);
+    if (!err) {
+        *value = i32;
+    }
+    return err;
 }
 
 int bq769x2_subcmd_read_f4(const uint16_t subcmd, float *value)
 {
-    return bq769x2_subcmd_read(subcmd, (uint32_t *)value, 4);
+    float f32;
+    int err = bq769x2_subcmd_read(subcmd, (uint32_t *)&f32, 4);
+    if (!err) {
+        *value = f32;
+    }
+    return err;
 }
 
 int bq769x2_subcmd_write_u1(const uint16_t subcmd, uint8_t value)
